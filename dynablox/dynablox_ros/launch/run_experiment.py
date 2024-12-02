83d089b89a5fa33d38e807cda9ed101ebc767caa
Stachq1 <stanislawpiasecki@protonmail.ch>
2024-12-02 16:17:23 +0100

    Add the option to not play rosbag in the launch file

--- dynablox/dynablox_ros/launch/run_experiment.py
+++ dynablox/dynablox_ros/launch/run_experiment.py
@@ -1,31 +1,48 @@
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription
+from launch.conditions import IfCondition
 from launch_ros.actions import Node
 from launch.substitutions import LaunchConfiguration
-from launch.conditions import IfCondition
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-  # Declare the arguments
-  config_file = os.path.join(get_package_share_directory('dynablox_ros'), 'config', 'motion_detector', 'default.yaml')
+  play_rosbag_flag = DeclareLaunchArgument(
+    'play_rosbag',
+    default_value='false',
+    description='Flag to determine if the rosbag should be played'
+  )
 
-  # Include play_dynablox_data.launch
+  config_file = os.path.join(
+      get_package_share_directory('dynablox_ros'),
+    'config',
+    'motion_detector',
+    'default.yaml'
+  )
+
   play_dynablox_data_launch = IncludeLaunchDescription(
     PythonLaunchDescriptionSource(
-      os.path.join(get_package_share_directory('dynablox_ros'), 'launch', 'play_dynablox_data.py')
+      os.path.join(
+        get_package_share_directory('dynablox_ros'),
+        'launch',
+        'play_dynablox_data.py'
+      )
     ),
-    launch_arguments={'bag_file': '/root/rosbags/indoor/indoor.db3', 'player_rate': '1'}.items()
+    launch_arguments={
+      'bag_file': '/root/rosbags/indoor/indoor.db3', 
+      'player_rate': '1'
+    }.items(),
+    condition=IfCondition(LaunchConfiguration('play_rosbag'))
   )
 
   motion_detector_node = Node(
-    package='dynablox_ros',
-    executable='motion_detector_node',
-    name='motion_detector_node',
-    output='screen',
-    arguments=['--alsologtostderr'],
-    parameters=[config_file]
+      package='dynablox_ros',
+      executable='motion_detector_node',
+      name='motion_detector_node',
+      output='screen',
+      arguments=['--alsologtostderr'],
+      parameters=[config_file]
   )
 
   rviz_node = Node(
@@ -35,6 +52,7 @@ def generate_launch_description():
   )
 
   return LaunchDescription([
+    play_rosbag_flag,
     play_dynablox_data_launch,
     motion_detector_node,
     rviz_node
