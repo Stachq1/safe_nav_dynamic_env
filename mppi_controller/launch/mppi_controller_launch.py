40842d61105eef905f970670dbd5660bf8d95c87
Stachq1 <stanislawpiasecki@protonmail.ch>
2024-12-19 13:23:11 +0100

    Add config file for simple setting and loading controller parameters

--- mppi_controller/launch/mppi_controller_launch.py
+++ mppi_controller/launch/mppi_controller_launch.py
@@ -1,15 +1,21 @@
-import launch
+import os
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
+    config_file = os.path.join(
+        get_package_share_directory('mppi_controller'),
+        'config',
+        'default.yaml'
+    )
+
     return LaunchDescription([
-        # Node for the MPPIController
         Node(
             package='mppi_controller',
             executable='mppi_controller_node',
             name='mppi_controller',
             output='screen',
-            parameters=[]
+            parameters=[config_file]
         )
     ])
